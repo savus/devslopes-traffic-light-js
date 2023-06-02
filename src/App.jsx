c4b01966b5d99f7f31d2ba4eac0aa18f2f1c5e48
jjhiggz <jonathan.higger@gmail.com>
2023-06-02 14:44:20 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { ClassTrafficLight } from "./ClassTrafficLight";
+import { FunctionalTrafficLight } from "./FunctionalTrafficLight";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Traffic light App</h1>
+        <div className="container">
+          <div className="left">
+            <FunctionalTrafficLight />
+          </div>
+          <div className="right">
+            <ClassTrafficLight />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
