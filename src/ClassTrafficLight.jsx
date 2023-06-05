241f1cf8037d0c2e17a9b42ba60f2ce81dfe4096
Savus <justin.fuzzygreenstuff@gmail.com>
2023-06-04 22:40:51 -0600

    Add state change functionality to Next State buttons of both ClassTrafficLight class component and FunctionalTrafficLight functional component.

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,37 @@
 import { Component } from "react";
 
+const colors = ["green", "yellow", "red"];
+
 export class ClassTrafficLight extends Component {
+  state = {
+    colorIndex: 2,
+  };
+
+  setColor = (index) =>
+    index === this.state.colorIndex ? colors[this.state.colorIndex] : "black";
+
+  incrementIndex = () => {
+    const isLast = (this.state.colorIndex === colors.length - 1);
+    if (isLast) {
+      this.setState({ colorIndex: 0 });
+    } else {
+      this.setState({ colorIndex: this.state.colorIndex + 1 });
+    }
+  };
+
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${this.setColor(2)}`}></div>
+          <div className={`circle ${this.setColor(1)}`}></div>
+          <div className={`circle ${this.setColor(0)}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.incrementIndex}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
+const colors = ["green", "yellow", "red"];
+
 export const FunctionalTrafficLight = () => {
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const setColor = (index) => index === colorIndex ? colors[colorIndex] : "black";
+
+  const incrementIndex = () => {
+    const isLast = colorIndex === colors.length - 1;
+    if (isLast) {
+      setColorIndex(colorIndex - colorIndex);
+    } else {
+      setColorIndex(colorIndex + 1);
+    }
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${setColor(2)}`}></div>
+        <div className={`circle ${setColor(1)}`}></div>
+        <div className={`circle ${setColor(0)}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={incrementIndex}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
